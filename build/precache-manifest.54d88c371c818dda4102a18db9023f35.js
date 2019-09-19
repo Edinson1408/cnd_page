ed94990e8f131e6f5b7d04cff1ae375ecd9a1304
Edinson1408 <edinsoncarlosflorian@gmail.com>
2019-09-19 07:13:21 -0500

    pagina conpilada react

---
+++ build/precache-manifest.54d88c371c818dda4102a18db9023f35.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0c4de76d9f9977728482bf02abec93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e33489ccf6a3c49dfb8",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "c45dbc51e6fe26721553",
+    "url": "/static/css/main.22683c2e.chunk.css"
+  },
+  {
+    "revision": "1e33489ccf6a3c49dfb8",
+    "url": "/static/js/2.833d4fd8.chunk.js"
+  },
+  {
+    "revision": "c45dbc51e6fe26721553",
+    "url": "/static/js/main.359eae7a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "15886877e091408f4d341466c5304bb5",
+    "url": "/static/media/Edinson.15886877.jpeg"
+  },
+  {
+    "revision": "9915d7f00b510d66b8beb929ca45b289",
+    "url": "/static/media/Emer.9915d7f0.jpeg"
+  },
+  {
+    "revision": "02b306e382bc597a5103b05bba91e939",
+    "url": "/static/media/Geral.02b306e3.jpeg"
+  },
+  {
+    "revision": "59d7bf871b46ef3559d6e871848dab75",
+    "url": "/static/media/GoPichanga.59d7bf87.jpg"
+  },
+  {
+    "revision": "cf9ea2565c1f2e865f537b4cae8f3645",
+    "url": "/static/media/Logo.cf9ea256.png"
+  },
+  {
+    "revision": "591cfa9333d7fed57e72950431d24d21",
+    "url": "/static/media/Vision.591cfa93.png"
+  },
+  {
+    "revision": "e62fd4afee0ac3731904ce3ecfeaef4b",
+    "url": "/static/media/background1.e62fd4af.jpg"
+  },
+  {
+    "revision": "9592778c3c5c678e9d43b257553f507a",
+    "url": "/static/media/background2.9592778c.jpg"
+  },
+  {
+    "revision": "5067b3f9cd609fb02d3cf8042c326e90",
+    "url": "/static/media/background3.5067b3f9.jpg"
+  },
+  {
+    "revision": "552022453b43d111d62efce0929ce243",
+    "url": "/static/media/fondo1.55202245.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
